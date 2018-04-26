280c964211f4dd8e090f085bbf8009817f8a64a5
Florian Mounier <florian.mounier@kozea.fr>
2018-04-26 16:20:35 +0200

    Add a bit of storybook configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,13 @@
 import '@storybook/addon-console'
 
+import { setOptions } from '@storybook/addon-options'
 import { configure } from '@storybook/react'
+
+setOptions({
+  name: 'Formol',
+  url: 'https://github.com/Kozea/formol',
+  addonPanelInRight: true,
+})
 
 function loadStories() {
   require('../stories/index.jsx')
