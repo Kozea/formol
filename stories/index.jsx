338b38680b8ba3c54a5c73df6c7b6a5ee02444dd
Florian Mounier <florian.mounier@kozea.fr>
2018-05-22 15:50:33 +0200

    Add a fun test-case

--- stories/index.jsx
+++ stories/index.jsx
@@ -156,6 +156,57 @@ storiesOf('Conditionals', module)
       </Formol>
     ))
   )
+  .add(
+    'Simple Conditional on props',
+    withStateForm(props => (
+      <Formol {...props}>
+        <h1>Complex field validation</h1>
+        <Field name="areyouok" type="switch">
+          Are you ok?
+        </Field>
+        <Conditional disabled={({ areyouok }) => areyouok}>
+          <Field name="why">Why?</Field>
+        </Conditional>
+        <Conditional disabled={({ areyouok }) => !areyouok}>
+          <Field name="whynot">Why not?</Field>
+        </Conditional>
+      </Formol>
+    ))
+  )
+  .add(
+    'Grouped conditionals',
+    withStateForm(props => (
+      <Formol {...props}>
+        <h1>Complex field validation</h1>
+        <Field name="areyouok" type="switch">
+          Are you ok?
+        </Field>
+        <Conditional show={({ areyouok }) => areyouok}>
+          <Field name="why">Why?</Field>
+          <Field name="tellmemore">Tell me more</Field>
+        </Conditional>
+        <Conditional show={({ areyouok }) => !areyouok}>
+          <Field name="whynot">Why not?</Field>
+          <Field name="iwilltryagain" type="checkbox">
+            I will try again
+          </Field>
+        </Conditional>
+      </Formol>
+    ))
+  )
+  .add(
+    'Original conditionals',
+    withStateForm(props => (
+      <Formol {...props}>
+        <h1>Even name can be used on conditional</h1>
+        <small>Even though it might not be a good idea</small>
+        <Field name="name">What’s your name?</Field>
+        <Conditional name={({ name }) => name || 'none'}>
+          <Field name="none">Say something</Field>
+        </Conditional>{' '}
+      </Formol>
+    ))
+  )
 
 storiesOf('Field Test', module)
 
