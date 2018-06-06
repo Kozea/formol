a4e0c46abbda35f24dba4ebbe982760d2a2f60a3
Florian Mounier <florian.mounier@kozea.fr>
2018-06-06 15:13:59 +0200

    Fix range field

--- src/fields/RangeField.jsx
+++ src/fields/RangeField.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { block } from '../utils'
-import NumberField from './BooleanField'
+import NumberField from './NumberField'
 
 @block
 export default class RangeField extends React.Component {
