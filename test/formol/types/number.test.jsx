a70290f52a416bd215bf85fb5bbac8162e40c8c4
Florian Mounier <florian.mounier@kozea.fr>
2018-07-10 16:54:40 +0200

    NumberField tests

--- test/formol/types/number.test.jsx
+++ test/formol/types/number.test.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 import Formol, { Field } from '../../../src'
 
-describe('Email field', () => {
+describe('Number field', () => {
   it('handles changes', async () => {
     const onSubmit = jest.fn()
     const wrapper = mount(
-      <Formol onSubmit={onSubmit} item={{ email: 'foo@bar.baz' }}>
-        <Field type="email">Email</Field>
+      <Formol onSubmit={onSubmit} item={{ number: 42 }}>
+        <Field type="number">Number</Field>
       </Formol>
     )
     const input = () =>
@@ -21,33 +21,31 @@ describe('Email field', () => {
 
     expect(submit().props().disabled).toBeTruthy()
     expect(cancel().props().disabled).toBeTruthy()
-    expect(input().props().value).toEqual('foo@bar.baz')
+    expect(input().props().value).toEqual(42)
 
     await input().simulate('focus')
     await input().simulate('change', {
-      target: { value: 'fuu@bar.baz' },
+      target: { value: 1337 },
     })
     await input().simulate('blur')
 
-    expect(input().props().value).toEqual('fuu@bar.baz')
+    expect(input().props().value).toEqual(1337)
     expect(submit().props().disabled).toBeFalsy()
     expect(cancel().props().disabled).toBeFalsy()
 
     await submit().simulate('click')
 
     expect(onSubmit).toHaveBeenCalled()
-    expect(onSubmit).toHaveBeenCalledWith(
-      { email: 'fuu@bar.baz' },
-      { email: 'foo@bar.baz' },
-      ['email']
-    )
-    expect(input().props().value).toEqual('fuu@bar.baz')
+    expect(onSubmit).toHaveBeenCalledWith({ number: 1337 }, { number: 42 }, [
+      'number',
+    ])
+    expect(input().props().value).toEqual(1337)
   })
   it('cancels changes', async () => {
     const onSubmit = jest.fn()
     const wrapper = mount(
-      <Formol onSubmit={onSubmit} item={{ email: 'foo@bar.baz' }}>
-        <Field type="email">Email</Field>
+      <Formol onSubmit={onSubmit} item={{ number: 42 }}>
+        <Field type="number">Number</Field>
       </Formol>
     )
     const input = () =>
@@ -60,27 +58,29 @@ describe('Email field', () => {
 
     expect(submit().props().disabled).toBeTruthy()
     expect(cancel().props().disabled).toBeTruthy()
-    expect(input().props().value).toEqual('foo@bar.baz')
+    expect(input().props().value).toEqual(42)
 
     await input().simulate('focus')
     await input().simulate('change', {
-      target: { value: 'fuu@bar.baz' },
+      target: { value: 1337 },
     })
     await input().simulate('blur')
 
-    expect(input().props().value).toEqual('fuu@bar.baz')
+    expect(input().props().value).toEqual(1337)
     expect(submit().props().disabled).toBeFalsy()
     expect(cancel().props().disabled).toBeFalsy()
 
     await cancel().simulate('click')
 
-    expect(input().props().value).toEqual('foo@bar.baz')
+    expect(input().props().value).toEqual(42)
   })
   it('prevents submission on field error', async () => {
     const onSubmit = jest.fn()
     const wrapper = mount(
-      <Formol onSubmit={onSubmit} item={{ email: 'foo@bar.baz' }}>
-        <Field type="email">Email</Field>
+      <Formol onSubmit={onSubmit} item={{ number: 42 }}>
+        <Field type="number" max={2000}>
+          Number
+        </Field>
       </Formol>
     )
     const input = () =>
@@ -93,18 +93,19 @@ describe('Email field', () => {
 
     expect(submit().props().disabled).toBeTruthy()
     expect(cancel().props().disabled).toBeTruthy()
-    expect(input().props().value).toEqual('foo@bar.baz')
+    expect(input().props().value).toEqual(42)
 
     await input().simulate('focus')
     await input().simulate('change', {
-      target: { value: 'not an email' },
+      target: { value: 4000 },
     })
     await input().simulate('blur')
+
     expect(wrapper.find('.Formol_Field__error-text').text()).toEqual(
       'Constraints not satisfied'
     )
 
-    expect(input().props().value).toEqual('not an email')
+    expect(input().props().value).toEqual(4000)
     expect(submit().props().disabled).toBeFalsy()
     expect(cancel().props().disabled).toBeFalsy()
 
@@ -112,11 +113,11 @@ describe('Email field', () => {
 
     expect(onSubmit).not.toHaveBeenCalled()
 
-    expect(input().props().value).toEqual('not an email')
+    expect(input().props().value).toEqual(4000)
 
     await input().simulate('focus')
     await input().simulate('change', {
-      target: { value: 'bar@bar.bar' },
+      target: { value: 1664 },
     })
     await input().simulate('blur')
     expect(wrapper.find('.Formol_Field__error-text').length).toEqual(0)
@@ -124,11 +125,36 @@ describe('Email field', () => {
     await submit().simulate('click')
 
     expect(onSubmit).toHaveBeenCalled()
-    expect(onSubmit).toHaveBeenCalledWith(
-      { email: 'bar@bar.bar' },
-      { email: 'foo@bar.baz' },
-      ['email']
+    expect(onSubmit).toHaveBeenCalledWith({ number: 1664 }, { number: 42 }, [
+      'number',
+    ])
+    expect(input().props().value).toEqual(1664)
+  })
+  it('prevents change when not a number', async () => {
+    const onSubmit = jest.fn()
+    const wrapper = mount(
+      <Formol onSubmit={onSubmit} item={{ number: 42 }}>
+        <Field type="number">Number</Field>
+      </Formol>
     )
-    expect(input().props().value).toEqual('bar@bar.bar')
+    const input = () =>
+      wrapper
+        .find('Field')
+        .find('input')
+        .first()
+    const submit = () => wrapper.find('.Formol_Formol__submit')
+    const cancel = () => wrapper.find('.Formol_Formol__cancel')
+
+    expect(submit().props().disabled).toBeTruthy()
+    expect(cancel().props().disabled).toBeTruthy()
+    expect(input().props().value).toEqual(42)
+
+    await input().simulate('focus')
+    await input().simulate('change', {
+      target: { value: 'not a number' },
+    })
+    await input().simulate('blur')
+
+    expect(input().props().value).toEqual('')
   })
 })
