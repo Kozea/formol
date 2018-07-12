a889e5e2f87141548b9bfe7abf3bba640c0708d3
Florian Mounier <florian.mounier@kozea.fr>
2018-07-12 16:18:24 +0200

    Add a default validator on color

--- src/fields/ColorField.jsx
+++ src/fields/ColorField.jsx
@@ -5,6 +5,10 @@ import InputField from './InputField'
 
 @block
 export default class ColorField extends React.PureComponent {
+  static defaultFieldProps = {
+    validator: () => v =>
+      v && !v.match(/#[0-9bA-Fa-f]{6}/) && `${v} is not a valid color`,
+  }
   render(b) {
     const { className, ...props } = this.props
     return <InputField className={b.mix(className)} {...props} />
