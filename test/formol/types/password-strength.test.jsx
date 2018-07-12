60ba56bb297d594f197b92323a00c10a284432f5
Florian Mounier <florian.mounier@kozea.fr>
2018-07-12 11:34:45 +0200

    Test password-strength field

---
+++ test/formol/types/password-strength.test.jsx
@@ -0,0 +1,213 @@
+import { mount } from 'enzyme'
+import React from 'react'
+
+import Formol, { Field } from '../../../src'
+
+describe('Password Strength field', () => {
+  it('handles changes', async () => {
+    const onSubmit = jest.fn()
+    const wrapper = mount(
+      <Formol onSubmit={onSubmit} item={{ password: 'r"/*Nb+4)23' }}>
+        <Field type="password-strength">Password</Field>
+      </Formol>
+    )
+    const asyncWrapper = () => wrapper.find('AsyncWrapper')
+    expect(asyncWrapper().text()).toEqual('Loading')
+    await asyncWrapper().instance()._promise
+    wrapper.update()
+    expect(asyncWrapper().text()).not.toEqual('Loading')
+
+    const input = () =>
+      wrapper
+        .find('Field')
+        .find('input')
+        .first()
+    const submit = () => wrapper.find('.Formol_Formol__submit')
+    const cancel = () => wrapper.find('.Formol_Formol__cancel')
+
+    expect(submit().props().disabled).toBeTruthy()
+    expect(cancel().props().disabled).toBeTruthy()
+    expect(input().props().type).toEqual('password')
+    expect(input().props().value).toEqual('r"/*Nb+4)23')
+
+    await input().simulate('focus')
+    await input().simulate('change', {
+      target: { value: 'u#@*/p=u58+e' },
+    })
+    await input().simulate('blur')
+    expect(input().props().value).toEqual('u#@*/p=u58+e')
+    expect(submit().props().disabled).toBeFalsy()
+    expect(cancel().props().disabled).toBeFalsy()
+
+    await submit().simulate('click')
+
+    expect(onSubmit).toHaveBeenCalled()
+    expect(onSubmit).toHaveBeenCalledWith(
+      { password: 'u#@*/p=u58+e' },
+      { password: 'r"/*Nb+4)23' },
+      ['password']
+    )
+    expect(input().props().value).toEqual('u#@*/p=u58+e')
+  })
+  it('cancels changes', async () => {
+    const onSubmit = jest.fn()
+    const wrapper = mount(
+      <Formol onSubmit={onSubmit} item={{ password: 'r"/*Nb+4)23' }}>
+        <Field type="password-strength">Password</Field>
+      </Formol>
+    )
+    const asyncWrapper = () => wrapper.find('AsyncWrapper')
+    expect(asyncWrapper().text()).toEqual('Loading')
+    await asyncWrapper().instance()._promise
+    wrapper.update()
+    expect(asyncWrapper().text()).not.toEqual('Loading')
+
+    const input = () =>
+      wrapper
+        .find('Field')
+        .find('input')
+        .first()
+    const submit = () => wrapper.find('.Formol_Formol__submit')
+    const cancel = () => wrapper.find('.Formol_Formol__cancel')
+
+    expect(submit().props().disabled).toBeTruthy()
+    expect(cancel().props().disabled).toBeTruthy()
+    expect(input().props().value).toEqual('r"/*Nb+4)23')
+
+    await input().simulate('focus')
+    await input().simulate('change', {
+      target: { value: 'u#@*/p=u58+e' },
+    })
+    await input().simulate('blur')
+
+    expect(input().props().value).toEqual('u#@*/p=u58+e')
+    expect(submit().props().disabled).toBeFalsy()
+    expect(cancel().props().disabled).toBeFalsy()
+
+    await cancel().simulate('click')
+
+    expect(input().props().value).toEqual('r"/*Nb+4)23')
+  })
+  it('shows password on eye click', async () => {
+    const wrapper = mount(
+      <Formol item={{ password: 'r"/*Nb+4)23' }}>
+        <Field type="password-strength">Password</Field>
+      </Formol>
+    )
+    const asyncWrapper = () => wrapper.find('AsyncWrapper')
+    expect(asyncWrapper().text()).toEqual('Loading')
+    await asyncWrapper().instance()._promise
+    wrapper.update()
+    expect(asyncWrapper().text()).not.toEqual('Loading')
+
+    const input = () =>
+      wrapper
+        .find('Field')
+        .find('input')
+        .first()
+    const eye = () => wrapper.find('Field').find('.Formol_PasswordField__eye')
+
+    expect(input().props().value).toEqual('r"/*Nb+4)23')
+    expect(input().props().type).toEqual('password')
+    await eye().simulate('click')
+    expect(input().props().value).toEqual('r"/*Nb+4)23')
+    expect(input().props().type).toEqual('text')
+
+    await input().simulate('focus')
+    await input().simulate('change', {
+      target: { value: 'u#@*/p=u58+e' },
+    })
+    await input().simulate('blur')
+    // bluring the field should restore password type
+    expect(input().props().type).toEqual('password')
+    expect(input().props().value).toEqual('u#@*/p=u58+e')
+
+    await eye().simulate('click')
+
+    expect(input().props().value).toEqual('u#@*/p=u58+e')
+    expect(input().props().type).toEqual('text')
+
+    await eye().simulate('click')
+
+    expect(input().props().value).toEqual('u#@*/p=u58+e')
+    expect(input().props().type).toEqual('password')
+  })
+  it('detects and prevents submission of weak passwords', async () => {
+    const onSubmit = jest.fn()
+    const wrapper = mount(
+      <Formol onSubmit={onSubmit} item={{ password: 'r"/*Nb+4)23' }}>
+        <Field type="password-strength">Password</Field>
+      </Formol>
+    )
+    const asyncWrapper = () => wrapper.find('AsyncWrapper')
+    expect(asyncWrapper().text()).toEqual('Loading')
+    await asyncWrapper().instance()._promise
+    wrapper.update()
+    expect(asyncWrapper().text()).not.toEqual('Loading')
+
+    const input = () =>
+      wrapper
+        .find('Field')
+        .find('input')
+        .first()
+    const submit = () => wrapper.find('.Formol_Formol__submit')
+    const cancel = () => wrapper.find('.Formol_Formol__cancel')
+    const strength = () =>
+      wrapper.find('.Formol_PasswordStrengthField__description').text()
+    const error = () => wrapper.find('.Formol_Field__error-text').text()
+
+    expect(submit().props().disabled).toBeTruthy()
+    expect(cancel().props().disabled).toBeTruthy()
+    expect(input().props().type).toEqual('password')
+    expect(input().props().value).toEqual('r"/*Nb+4)23')
+    expect(strength()).toEqual('stronger')
+
+    await input().simulate('focus')
+    await input().simulate('change', {
+      target: { value: 'passw0rd' },
+    })
+    expect(strength()).toEqual('weak')
+    await input().simulate('blur')
+    expect(error()).toEqual('Please choose a more secure password.')
+
+    expect(input().props().value).toEqual('passw0rd')
+    expect(submit().props().disabled).toBeFalsy()
+    expect(cancel().props().disabled).toBeFalsy()
+
+    await submit().simulate('click')
+
+    expect(onSubmit).not.toHaveBeenCalled()
+
+    await input().simulate('focus')
+    await input().simulate('change', {
+      target: { value: 'iamsecret' },
+    })
+    expect(strength()).toEqual('okay')
+    await input().simulate('blur')
+    expect(error()).toEqual('Please choose a more secure password.')
+
+    expect(input().props().value).toEqual('iamsecret')
+    expect(submit().props().disabled).toBeFalsy()
+    expect(cancel().props().disabled).toBeFalsy()
+
+    await submit().simulate('click')
+
+    expect(onSubmit).not.toHaveBeenCalled()
+
+    await input().simulate('focus')
+    await input().simulate('change', {
+      target: { value: '2sht' },
+    })
+    expect(strength()).toEqual('too short')
+    await input().simulate('blur')
+    expect(error()).toEqual('Please choose a more secure password.')
+
+    expect(input().props().value).toEqual('2sht')
+    expect(submit().props().disabled).toBeFalsy()
+    expect(cancel().props().disabled).toBeFalsy()
+
+    await submit().simulate('click')
+
+    expect(onSubmit).not.toHaveBeenCalled()
+  })
+})
