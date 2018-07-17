7eb99d37dd6ab51fa4a0e78d601b90829d00c3f9
Florian Mounier <florian.mounier@kozea.fr>
2018-07-17 10:55:29 +0200

    Add switch test

---
+++ test/formol/types/switch.test.jsx
@@ -0,0 +1,74 @@
+import { mount } from 'enzyme'
+import React from 'react'
+
+import Formol, { Field } from '../../../src'
+
+describe('Switch field', () => {
+  it('handles changes', async () => {
+    const onSubmit = jest.fn()
+    const wrapper = mount(
+      <Formol onSubmit={onSubmit} item={{ switch: false }}>
+        <Field type="switch">Switch</Field>
+      </Formol>
+    )
+    const input = () =>
+      wrapper
+        .find('Field')
+        .find('input')
+        .first()
+    const submit = () => wrapper.find('.Formol_Formol__submit')
+    const cancel = () => wrapper.find('.Formol_Formol__cancel')
+
+    expect(submit().props().disabled).toBeTruthy()
+    expect(cancel().props().disabled).toBeTruthy()
+    expect(input().props().type).toEqual('checkbox')
+    expect(input().props().checked).toEqual(false)
+
+    await input().simulate('focus')
+    await input().simulate('change', { target: { checked: true } })
+    await input().simulate('blur')
+
+    expect(input().props().checked).toEqual(true)
+    expect(submit().props().disabled).toBeFalsy()
+    expect(cancel().props().disabled).toBeFalsy()
+
+    await submit().simulate('click')
+
+    expect(onSubmit).toHaveBeenCalled()
+    expect(onSubmit).toHaveBeenCalledWith({ switch: true }, { switch: false }, [
+      'switch',
+    ])
+    expect(input().props().checked).toEqual(true)
+  })
+  it('cancels changes', async () => {
+    const onSubmit = jest.fn()
+    const wrapper = mount(
+      <Formol onSubmit={onSubmit} item={{ switch: false }}>
+        <Field type="switch">Switch</Field>
+      </Formol>
+    )
+    const input = () =>
+      wrapper
+        .find('Field')
+        .find('input')
+        .first()
+    const submit = () => wrapper.find('.Formol_Formol__submit')
+    const cancel = () => wrapper.find('.Formol_Formol__cancel')
+
+    expect(submit().props().disabled).toBeTruthy()
+    expect(cancel().props().disabled).toBeTruthy()
+    expect(input().props().checked).toEqual(false)
+
+    await input().simulate('focus')
+    await input().simulate('change', { target: { checked: true } })
+    await input().simulate('blur')
+
+    expect(input().props().checked).toEqual(true)
+    expect(submit().props().disabled).toBeFalsy()
+    expect(cancel().props().disabled).toBeFalsy()
+
+    await cancel().simulate('click')
+
+    expect(input().props().checked).toEqual(false)
+  })
+})
