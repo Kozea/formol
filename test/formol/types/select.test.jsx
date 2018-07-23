02e28d4aae575891e8a07ff49c141cef84184641
Florian Mounier <florian.mounier@kozea.fr>
2018-07-23 17:48:42 +0200

    Test select $multiple fields

--- test/formol/types/select.test.jsx
+++ test/formol/types/select.test.jsx
@@ -149,4 +149,166 @@ describe('Select field', () => {
 
     expect(select().props().value).toEqual('##formol_memo_0')
   })
+  it('handles changes with multiple values', async () => {
+    const onSubmit = jest.fn()
+    const wrapper = mount(
+      <Formol onSubmit={onSubmit} item={{ select: [1, 'II'] }}>
+        <Field
+          type="select"
+          multiple
+          choices={{
+            one: 1,
+            two: 'II',
+            three: true,
+          }}
+        >
+          Select
+        </Field>
+      </Formol>
+    )
+    const select = () => wrapper.find('Field').find('select')
+
+    const submit = () => wrapper.find('.Formol_Formol__submit')
+    const cancel = () => wrapper.find('.Formol_Formol__cancel')
+
+    expect(submit().props().disabled).toBeTruthy()
+    expect(cancel().props().disabled).toBeTruthy()
+
+    // There's no empty option in multiple
+    expect(select().children()).toHaveLength(3)
+
+    select()
+      .children()
+      .forEach(option => expect(option.type()).toEqual('option'))
+    expect(
+      select()
+        .children()
+        .at(0)
+        .text()
+    ).toEqual('one')
+    expect(
+      select()
+        .children()
+        .at(1)
+        .text()
+    ).toEqual('two')
+    expect(
+      select()
+        .children()
+        .at(2)
+        .text()
+    ).toEqual('three')
+    expect(
+      select()
+        .children()
+        .at(0)
+        .props().value
+    ).toEqual('##formol_memo_0')
+    expect(
+      select()
+        .children()
+        .at(1)
+        .props().value
+    ).toEqual('II')
+    expect(
+      select()
+        .children()
+        .at(2)
+        .props().value
+    ).toEqual('##formol_memo_2')
+
+    expect(select().props().value).toEqual(['##formol_memo_0', 'II'])
+    // Simulating removal of first property and the add of the last
+    await select().simulate('focus')
+    await select().simulate('change', {
+      target: {
+        options: select()
+          .children()
+          .map((option, i) => ({ ...option.props(), selected: i > 0 })),
+      },
+    })
+    await select().simulate('blur')
+
+    expect(submit().props().disabled).toBeFalsy()
+    expect(cancel().props().disabled).toBeFalsy()
+
+    expect(select().props().value).toEqual(['II', '##formol_memo_2'])
+
+    await submit().simulate('click')
+
+    expect(onSubmit).toHaveBeenCalled()
+    expect(onSubmit).toHaveBeenCalledWith(
+      { select: ['II', true] },
+      { select: [1, 'II'] },
+      ['select']
+    )
+    expect(select().props().value).toEqual(['II', '##formol_memo_2'])
+  })
+  it('cancels changes in multiple', async () => {
+    const onSubmit = jest.fn()
+    const wrapper = mount(
+      <Formol onSubmit={onSubmit} item={{ select: [1, 'II'] }}>
+        <Field
+          type="select"
+          multiple
+          choices={{
+            one: 1,
+            two: 'II',
+            three: true,
+          }}
+        >
+          Select
+        </Field>
+      </Formol>
+    )
+    const select = () => wrapper.find('Field').find('select')
+
+    const submit = () => wrapper.find('.Formol_Formol__submit')
+    const cancel = () => wrapper.find('.Formol_Formol__cancel')
+
+    expect(submit().props().disabled).toBeTruthy()
+    expect(cancel().props().disabled).toBeTruthy()
+
+    expect(select().props().value).toEqual(['##formol_memo_0', 'II'])
+    // Simulating removal of first property and the add of the last
+    await select().simulate('focus')
+    await select().simulate('change', {
+      target: {
+        options: select()
+          .children()
+          .map((option, i) => ({ ...option.props(), selected: i > 0 })),
+      },
+    })
+    await select().simulate('blur')
+
+    expect(submit().props().disabled).toBeFalsy()
+    expect(cancel().props().disabled).toBeFalsy()
+
+    expect(select().props().value).toEqual(['II', '##formol_memo_2'])
+
+    await cancel().simulate('click')
+
+    expect(submit().props().disabled).toBeTruthy()
+    expect(cancel().props().disabled).toBeTruthy()
+
+    expect(select().props().value).toEqual(['##formol_memo_0', 'II'])
+  })
+  it('sets disabled when readOnly', () => {
+    const onSubmit = jest.fn()
+    const wrapper = mount(
+      <Formol onSubmit={onSubmit} item={{ select: 'II' }}>
+        <Field type="select" readOnly>
+          Checkbox
+        </Field>
+      </Formol>
+    )
+    const input = () => wrapper.find('Field').find('select')
+
+    const submit = () => wrapper.find('.Formol_Formol__submit')
+    const cancel = () => wrapper.find('.Formol_Formol__cancel')
+
+    expect(submit().props().disabled).toBeTruthy()
+    expect(cancel().props().disabled).toBeTruthy()
+    expect(input().props().disabled).toEqual(true)
+  })
 })
