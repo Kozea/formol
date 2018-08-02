7c2185c90adbd36bf92af87a1da969cfd96ddf0b
Florian Mounier <florian.mounier@kozea.fr>
2018-08-02 15:59:18 +0200

    Remove useless file

--- src/async/ReactDayPicker.jsx
+++
@@ -1,9 +0,0 @@
-import 'react-day-picker/lib/style.css'
-
-import Async from '../utils/Async'
-
-export default Async(() =>
-  import(/*
-    webpackChunkName: "DayPickerInput"
-  */ 'react-day-picker/DayPickerInput')
-)
