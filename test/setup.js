ee97fb79acc060f317bdc349738fe34de6c2f75e
Florian Mounier <florian.mounier@kozea.fr>
2018-08-09 15:27:02 +0200

    Update bemboo and upgrade enzyme so we don't need the react hack anymore.

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,4 @@
-import { defaultSettings } from 'bemboo'
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-defaultSettings.cache = false
 configure({ adapter: new Adapter() })
