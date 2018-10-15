a3cea231c99f1de060a92f7d7dc39848fcc347fb
Florian Mounier <florian.mounier@kozea.fr>
2018-10-15 10:21:45 +0200

    Fix MenuList once again

--- src/utils/MenuList.jsx
+++ src/utils/MenuList.jsx
@@ -7,54 +7,91 @@ export default (listDefaultHeight = 19, listApproximatedLengthBreak = 50) =>
   class MenuList extends React.PureComponent {
     constructor(props) {
       super(props)
-
       this.list = React.createRef()
+      this.itemSize = this.itemSize.bind(this)
+
+      this.state = {
+        rawChildren: null,
+        maxHeight: null,
+        _list: this.list,
+      }
     }
 
-    componentDidMount() {
-      this.list.current.resetAfterIndex(0)
+    static getDerivedStateFromProps(
+      { children: rawChildren, options, maxHeight, getValue },
+      state
+    ) {
+      if (rawChildren !== state.rawChildren || maxHeight !== state.maxHeight) {
+        const [value] = getValue()
+
+        const children = Array.isArray(rawChildren)
+          ? rawChildren
+          : [rawChildren]
+
+        // Waiting for https://github.com/bvaughn/react-window/issues/6
+        const heights = children.map(
+          ({ props: { label } }) =>
+            8 +
+            8 +
+            listDefaultHeight *
+              Math.ceil((label || '_').length / listApproximatedLengthBreak)
+        )
+        const totalHeight = heights.reduce(sum)
+        const height = Math.min(maxHeight, totalHeight)
+        const itemCount = children.length
+        // initialScrollOffset is wrong if the list is sorted
+        const currentIndex =
+          options.length === children.length ? options.indexOf(value) : 0
+        const initialScrollOffset = Math.min(
+          totalHeight - height,
+          currentIndex > 0 ? heights.slice(0, currentIndex).reduce(sum) : 0
+        )
+
+        const estimatedItemSize = Math.floor(totalHeight / itemCount)
+        if (initialScrollOffset !== state.initialScrollOffset) {
+          state._list.current &&
+            state._list.current.scrollTo(initialScrollOffset)
+        }
+
+        return {
+          height,
+          itemCount,
+          heights,
+          estimatedItemSize,
+          initialScrollOffset,
+          maxHeight,
+          rawChildren,
+        }
+      }
+      return null
+    }
+
+    itemSize(index) {
+      return this.state.heights[index]
     }
 
     render() {
-      let { children } = this.props
-      const { options, maxHeight, getValue, innerRef } = this.props
-      const [value] = getValue()
-
-      if (!Array.isArray(children)) {
-        children = [children]
-      }
-
-      // Waiting for https://github.com/bvaughn/react-window/issues/6
-      const heights = children.map(
-        ({ props: { label } }) =>
-          8 +
-          8 +
-          listDefaultHeight *
-            Math.ceil((label || '_').length / listApproximatedLengthBreak)
-      )
-      const totalHeight = heights.reduce(sum)
-      const height = Math.min(maxHeight, totalHeight)
-      const itemCount = children.length
-      // initialScrollOffset is wrong if the list is sorted
-      const currentIndex =
-        options.length === children.length ? options.indexOf(value) : 0
-      const initialScrollOffset =
-        currentIndex > 0 ? heights.slice(0, currentIndex).reduce(sum) : 0
-      const estimatedItemSize = Math.floor(totalHeight / itemCount)
-
-      const itemSize = index => heights[index]
-
+      const { children, innerRef } = this.props
+      const {
+        height,
+        itemCount,
+        estimatedItemSize,
+        initialScrollOffset,
+      } = this.state
+      const row = Array.isArray(children)
+        ? ({ index, style }) => <div style={style}>{children[index]}</div>
+        : ({ style }) => <div style={style}>{children}</div>
       return (
         <div ref={innerRef}>
           <VariableSizeList
             ref={this.list}
             height={height}
             itemCount={itemCount}
-            itemSize={itemSize}
+            itemSize={this.itemSize}
             estimatedItemSize={estimatedItemSize}
             initialScrollOffset={initialScrollOffset}
           >
-            {({ index, style }) => <div style={style}>{children[index]}</div>}
+            {row}
           </VariableSizeList>
         </div>
       )
