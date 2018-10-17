24c05c01e1adca7a846010330349678fdad5de47
Florian Mounier <florian.mounier@kozea.fr>
2018-10-17 16:24:22 +0200

    Keep scrolling to focused/selected element to fix #39 this is still underoptimized

--- src/utils/MenuList.jsx
+++ src/utils/MenuList.jsx
@@ -3,26 +3,33 @@ import { VariableSizeList } from 'react-window'
 
 const sum = (a, b) => a + b
 
+class OptionWrapper extends React.PureComponent {
+  render() {
+    const { style, index, data } = this.props
+    return <div style={style}>{data[index]}</div>
+  }
+}
+
 export default (listDefaultHeight = 19, listApproximatedLengthBreak = 50) =>
+  // eslint-disable-next-line react/no-multi-comp
   class MenuList extends React.PureComponent {
     constructor(props) {
       super(props)
       this.itemSize = this.itemSize.bind(this)
+      this.list = React.createRef()
 
       this.state = {
         rawChildren: null,
         maxHeight: null,
-        initialScrollOffset: 0,
+        currentIndex: 0,
       }
     }
 
     static getDerivedStateFromProps(
-      { children: rawChildren, options, maxHeight, getValue },
+      { children: rawChildren, maxHeight },
       state
     ) {
       if (rawChildren !== state.rawChildren || maxHeight !== state.maxHeight) {
-        const [value] = getValue()
-
         const children = Array.isArray(rawChildren)
           ? rawChildren
           : [rawChildren]
@@ -38,27 +45,39 @@ export default (listDefaultHeight = 19, listApproximatedLengthBreak = 50) =>
         const totalHeight = heights.reduce(sum)
         const height = Math.min(maxHeight, totalHeight)
         const itemCount = children.length
-        // initialScrollOffset is wrong if the list is sorted
-        const currentIndex =
-          options.length === children.length ? options.indexOf(value) : 0
-        const initialScrollOffset = Math.min(
-          totalHeight - height,
-          currentIndex > 0 ? heights.slice(0, currentIndex).reduce(sum) : 0
+
+        const focusedIndex = children.findIndex(
+          ({ props: { isFocused } }) => isFocused
+        )
+
+        const currentIndex = Math.max(
+          0,
+          focusedIndex === -1
+            ? children.findIndex(({ props: { isSelected } }) => isSelected)
+            : focusedIndex
         )
 
         const estimatedItemSize = Math.floor(totalHeight / itemCount)
-
         return {
           height,
           itemCount,
           heights,
           estimatedItemSize,
-          initialScrollOffset,
           maxHeight,
           rawChildren,
+          currentIndex,
         }
       }
       return null
+    }
+
+    componentDidMount() {
+      this.componentDidUpdate()
+    }
+
+    componentDidUpdate() {
+      const { currentIndex } = this.state
+      this.list.current.scrollToItem(currentIndex)
     }
 
     itemSize(index) {
@@ -67,25 +86,18 @@ export default (listDefaultHeight = 19, listApproximatedLengthBreak = 50) =>
 
     render() {
       const { children, innerRef } = this.props
-      const {
-        height,
-        itemCount,
-        estimatedItemSize,
-        initialScrollOffset,
-      } = this.state
-      const row = Array.isArray(children)
-        ? ({ index, style }) => <div style={style}>{children[index]}</div>
-        : ({ style }) => <div style={style}>{children}</div>
+      const { height, itemCount, estimatedItemSize } = this.state
       return (
         <div ref={innerRef}>
           <VariableSizeList
+            ref={this.list}
             height={height}
             itemCount={itemCount}
             itemSize={this.itemSize}
             estimatedItemSize={estimatedItemSize}
-            initialScrollOffset={initialScrollOffset}
+            itemData={Array.isArray(children) ? children : [children]}
           >
-            {row}
+            {OptionWrapper}
           </VariableSizeList>
         </div>
       )
