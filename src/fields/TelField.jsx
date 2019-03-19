3c27de5c1c5ba534eac6a1cc8a46fb5da5630cf5
Florian Mounier <florian.mounier@kozea.fr>
2019-03-19 11:59:45 +0100

    Fix phone pattern field

--- src/fields/TelField.jsx
+++ src/fields/TelField.jsx
@@ -7,7 +7,7 @@ import InputField from './InputField'
 export default class TelField extends React.PureComponent {
   render(b) {
     const { className, pattern, ...props } = this.props
-    props.pattern = props.pattern || props.i18n.tel.pattern.source
+    props.pattern = pattern || props.i18n.tel.pattern.source
     return <InputField className={b.mix(className)} {...props} />
   }
 }
