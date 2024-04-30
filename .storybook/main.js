651203e9138c7deec464eef10eef76bbb4fceabc
Gilles Lepretre <gilles.lepretre@kozea.fr>
2024-04-30 18:58:14 +0200

    Remove @storybook/addon-webpack5-compiler-swc
    I don't wan't to use swc

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,6 @@
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-webpack5-compiler-swc',
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
