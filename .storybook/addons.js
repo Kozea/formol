801376429e71fb8d3b72b7c835a4f7db3d37e99e
Gilles Lepretre <gilles.lepretre@kozea.fr>
2024-05-03 17:49:43 +0200

    [PoC] Storybook 8

--- .storybook/addons.js
+++
@@ -1,5 +0,0 @@
-import 'storybook4-state/register'
-import '@storybook/addon-knobs/register'
-import '@storybook/addon-storysource/register'
-
-import '../storybook-addon-theme/src/register'
---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
