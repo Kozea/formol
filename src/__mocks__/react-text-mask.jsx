d0580a29fb389b7d3a91a25d6abf761077a09521
Gilles Lepretre <gilles.lepretre@kozea.fr>
2024-05-20 12:08:22 +0200

    Lint

--- src/__mocks__/react-text-mask.jsx
+++ src/__mocks__/react-text-mask.jsx
@@ -1,16 +1,14 @@
 import * as React from 'react'
 
 // https://github.com/text-mask/text-mask/issues/427#issuecomment-479945031
-const mock = React.forwardRef(
-  (props, ref) => {
-    const { render, ...otherProps } = props
+const mock = React.forwardRef((props, ref) => {
+  const { render, ...otherProps } = props
 
-    return render ? (
-      render(ref, { ...otherProps })
-    ) : (
-      <input ref={ref} {...otherProps} />
-    )
-  }
-)
+  return render ? (
+    render(ref, { ...otherProps })
+  ) : (
+    <input ref={ref} {...otherProps} />
+  )
+})
 
 export default mock
